f4b110d42719f94a64cb97931955bef11fbaa4f7
JWWon <wonjiwn@naver.com>
2018-10-17 13:09:45 +0900

    Complete setting About page

--- src/pages/About/About.styled.ts
+++ src/pages/About/About.styled.ts
@@ -1,7 +1,33 @@
+import { Link as _Link } from 'react-router-dom';
 import styled from 'theme';
 
 export const Container = styled.div`
   min-height: 60vh;
   display: flex;
   flex-direction: column;
+  margin-bottom: 6rem;
+`;
+
+const linkHeight = 4.8;
+
+export const Link = styled(_Link).attrs({
+  to: '/search',
+})`
+  margin: 0 auto;
+  width: 32rem;
+  box-sizing: border-box;
+  height: ${linkHeight}rem;
+  border-radius: ${linkHeight / 2}rem;
+  background: ${({ theme }) => theme.color.blueDark};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.font.size.m}rem;
+  &:hover {
+    background: ${({ theme }) => theme.color.white};
+    border: 2px solid ${({ theme }) => theme.color.blueDark};
+    color: ${({ theme }) => theme.color.blueDark};
+    transition: all 0.2s ease-out;
+  }
 `;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import { Editor, EditorState } from 'draft-js';
+import React, { Component } from 'react';
 
 import { Section, ShadowBox, TitleWithBar } from 'components/common';
 import * as s from './About.styled';
 
-const About = () => (
-  <Section title="잇츠교정이란?">
-    <s.Container>
-      <ShadowBox>
-        <TitleWithBar
-          title="치아 교정병원을 선택하는 가장 객관적인 기준"
-          margin="0.8rem 0 4rem"
-        />
-        <div>
-          <p>HELLO</p>
-        </div>
-      </ShadowBox>
-    </s.Container>
-  </Section>
-);
+interface State {
+  editorState: EditorState;
+}
+
+class About extends Component<{}, State> {
+  public state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  public render() {
+    return (
+      <Section title="잇츠교정이란?">
+        <s.Container>
+          <ShadowBox>
+            <TitleWithBar
+              title="치아 교정병원을 선택하는 가장 객관적인 기준"
+              margin="0.8rem 0 4rem"
+            />
+            <Editor
+              readOnly={true}
+              editorState={this.state.editorState}
+              onChange={this.handleChange}
+            />
+          </ShadowBox>
+        </s.Container>
+        <s.Link>검색 바로가기</s.Link>
+      </Section>
+    );
+  }
+
+  private handleChange = (editorState: EditorState) => {
+    console.log(editorState);
+  };
+}
 
 export default About;
