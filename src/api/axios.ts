b5c35ccf309d0c0a3bc1847d20f53f060e8106ef
JWWon <wonjiwn@naver.com>
2018-11-15 02:33:17 +0900

    Update npm scripts, Change baseURL for production

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://www.itskyojeong.com/api';
+  axios.defaults.baseURL = 'https://api.itskyojeong.com/';
 } else {
   axios.defaults.baseURL = 'http://localhost:5000/';
 }
