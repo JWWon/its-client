6414c921eb185ca1c810eef379d2f1efc576f0ee
JWWon <wonjiwn@naver.com>
2018-11-14 16:36:59 +0900

    Update api router

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://www.itso-o.com/api';
+  axios.defaults.baseURL = 'https://api.itso-o.com/';
 } else {
   axios.defaults.baseURL = 'http://localhost:5000/';
 }
+
+axios.defaults.headers.common['Content-Type'] =
+  'application/x-www-form-urlencoded';
