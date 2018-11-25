96c28b257d9b84f70c856f5e308c66f4b405c04e
JWWon <wonjiwn@naver.com>
2018-11-25 22:43:24 +0900

    Add create invalidation of cloudfront in package.json script

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+const rootComponent = (
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root') as HTMLElement
+  </BrowserRouter>
 );
-registerServiceWorker();
+const rootElement = document.getElementById('root') as HTMLElement;
+
+if (rootElement.hasChildNodes()) {
+  hydrate(rootComponent, rootElement);
+} else {
+  render(rootComponent, rootElement);
+}
+// registerServiceWorker();
